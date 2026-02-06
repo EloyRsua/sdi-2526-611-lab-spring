48d9a0a8e3c0f16e6bd172fd47b5210ddccb584e
EloyRsua <eloy.rubio.suarez@gmail.com>
2026-02-06 06:38:41 +0100

    SDI-2526-611-2.1-Creating Spring project.

---
+++ sdi-2526-611-spring/src/main/java/com/uniovi/sdi/sdi2526611spring/Sdi2526611SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2526611spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2526611SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2526611SpringApplication.class, args);
+    }
+
+}
---
+++ sdi-2526-611-spring/src/test/java/com/uniovi/sdi/sdi2526611spring/Sdi2526611SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2526611spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2526611SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
