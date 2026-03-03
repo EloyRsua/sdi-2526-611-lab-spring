3ef723d8955439d8bd5a5da4a94c73e5f4a4e047
EloyRsua <eloy.rubio.suarez@gmail.com>
2026-03-03 11:48:15 +0100

    SDI-IDGIT-sesión6-ejercicio-complementario1

--- sdi-2526-611-spring/src/test/java/com/uniovi/sdi/sdi2526611spring/pageobjects/PO_PrivateView.java
+++ sdi-2526-611-spring/src/test/java/com/uniovi/sdi/sdi2526611spring/pageobjects/PO_PrivateView.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class PO_PrivateView extends PO_NavView {
     static public void fillFormAddMark(WebDriver driver, int userOrder, String descriptionp, String scorep)
     {
@@ -23,5 +26,23 @@ public class PO_PrivateView extends PO_NavView {
         score.sendKeys(scorep);
         By boton = By.className("btn");
         driver.findElement(boton).click();
+    }
+    static public void goToLastPage(WebDriver driver) {
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//a[contains(@class, 'page-link')]");
+        elements.getLast().click();
+    }
+
+    static public void goToAddMarkPage(WebDriver driver) {
+        // Click en el menú de Notas
+        PO_View.checkElementBy(driver, "free", "//*[@id='myNavbar']/ul[1]/li[2]").getFirst().click();
+        // Click en Añadir Nota
+        PO_View.checkElementBy(driver, "free", "//a[contains(@href, 'mark/add')]").getFirst().click();
+    }
+
+    static public void goToListMarksPage(WebDriver driver) {
+        // Click en el menú de Notas
+        PO_View.checkElementBy(driver, "free", "//*[@id='myNavbar']/ul[1]/li[2]").getFirst().click();
+        // Click en Listado de notas
+        PO_View.checkElementBy(driver, "free", "//a[contains(@href, 'mark/list')]").getFirst().click();
     }
 }
